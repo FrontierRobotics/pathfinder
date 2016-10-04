816d41fd629f902693f5f7d035c6532487e578dd
Andy Condon <condon.andy@icloud.com>
2016-10-04 11:57:30 -0600

    Classes for the win!!!

---
+++ src/main/arduino/Motion/LCD.cpp
@@ -0,0 +1,105 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include "LCD.h"
+
+#define LCD_RX_PIN 30  // rxPin is immaterial - not used - just make this an unused Arduino pin number
+
+LCD::LCD(int txPin,byte rows, byte columns) : _serial(SoftwareSerial(LCD_RX_PIN, txPin)) {
+  pinMode(txPin, OUTPUT);
+  _rows = rows;
+  _columns = columns;
+}
+
+void LCD::begin() {
+  _serial.begin(9600);                // 9600 baud is chip comm speed
+  
+  set_geometry(_rows, _columns);
+  delay(500);                           // pause to allow LCD EEPROM to program
+
+  set_brightness(0xFF);
+  delay(1000);                          // pause to allow LCD EEPROM to program
+
+  print("?s6");                  // set tabs to six spaces
+  delay(1000);                          // pause to allow LCD EEPROM to program
+
+  print("?D00000000000000000");  // define special characters
+  delay(300);                           // delay to allow write to EEPROM
+  // see moderndevice.com for a handy custom char generator (software app)
+  clear_screen();
+  delay(10);
+  print("...");
+
+
+  //crashes LCD without delay
+  print("?D11010101010101010");
+  delay(300);
+
+  print("?D21818181818181818");
+  delay(300);
+
+  print("?D31c1c1c1c1c1c1c1c");
+  delay(300);
+
+  print("?D41e1e1e1e1e1e1e1e");
+  delay(300);
+
+  print("?D51f1f1f1f1f1f1f1f");
+  delay(300);
+
+  print("?D60000000000040E1F");
+  delay(300);
+
+  print("?D70000000103070F1F");
+  delay(300);
+
+  disable_cursor();
+  delay(300);
+}
+
+void LCD::clear_screen() {
+  print("?f");
+}
+
+void LCD::set_geometry(byte rows, byte columns) {
+  print("?G%d%02d", rows, columns);
+}
+
+void LCD::set_brightness(byte brightness) {
+  print("?B%02X", brightness);
+}
+
+void LCD::set_cursor(byte row, byte column) {
+  set_cursor_row(row);
+  set_cursor_column(column);
+}
+
+void LCD::set_cursor_row(byte row) {
+  print("?y%d", row);
+}
+
+void LCD::set_cursor_column(byte column) {
+  print("?x%02d", column);
+}
+
+/*
+void lcd_underline_cursor() {
+  lcd_print("?c3");
+}
+
+void lcd_blink_cursor() {
+  lcd_print("?c2");
+}
+*/
+
+void LCD::disable_cursor() {
+  print("?c0");
+}
+
+void LCD::print(char const *fmt, ... ) {
+  char buf[20];
+  va_list args;
+  va_start (args, fmt );
+  vsnprintf(buf, 20, fmt, args);
+  va_end (args);
+  _serial.print(buf);
+}
--- src/main/arduino/Motion/Motion.ino
+++ src/main/arduino/Motion/Motion.ino
@@ -1,27 +1,33 @@
 #include <Wire.h>
+#include "LCD.h"
 
 #define I2C_ADDRESS 0x1A
+#define LCD_TX_PIN 10
+#define LCD_ROWS 4
+#define LCD_COLUMNS 20
+
+LCD lcd = LCD(LCD_TX_PIN, LCD_ROWS, LCD_COLUMNS);
 
 void setup() {
   Wire.begin(I2C_ADDRESS);
   Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
-  lcd_initialize();
-  lcd_clear();
+  lcd.begin();
+  lcd.clear_screen();
 }
 
 void loop() {
-  lcd_set_brightness(0x00);
+  lcd.set_brightness(0x00);
   delay(1000);
-  lcd_set_brightness(0x22);
+  lcd.set_brightness(0x22);
   delay(1000);
-  lcd_set_brightness(0x44);
+  lcd.set_brightness(0x44);
   delay(1000);
-  lcd_set_brightness(0xFF);
+  lcd.set_brightness(0xFF);
   delay(1000);
 
-  lcd_set_cursor(1, 5);
-  lcd_print("Hi!");
+  lcd.set_cursor(1, 5);
+  lcd.print("Hi!");
   delay(1000);
 }
 
