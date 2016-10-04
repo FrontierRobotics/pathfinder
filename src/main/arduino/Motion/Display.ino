d8cb319e07761286e1ed24e56585f04e6b621991
Andy Condon <condon.andy@icloud.com>
2016-10-03 21:50:19 -0600

    Cleanup.

--- src/main/arduino/Motion/Display.ino
+++ src/main/arduino/Motion/Display.ino
@@ -17,48 +17,48 @@ int Num_5;
 
 void lcd_initialize() {
   pinMode(txPin, OUTPUT);
-  serial.begin(9600);      // 9600 baud is chip comm speed
+  serial.begin(9600);                // 9600 baud is chip comm speed
 
-  serial.print("?G216");   // set display geometry,  2 x 16 characters in this case
-  delay(500);                // pause to allow LCD EEPROM to program
+  lcd_print("?G216");                // set display geometry,  2 x 16 characters in this case
+  delay(500);                           // pause to allow LCD EEPROM to program
 
-  serial.print("?Bff");    // set backlight to ff hex, maximum brightness
-  delay(1000);                // pause to allow LCD EEPROM to program
+  lcd_set_brightness(0xFF);          // set backlight to ff hex, maximum brightness
+  delay(1000);                          // pause to allow LCD EEPROM to program
 
-  serial.print("?s6");     // set tabs to six spaces
-  delay(1000);               // pause to allow LCD EEPROM to program
+  lcd_print("?s6");                  // set tabs to six spaces
+  delay(1000);                          // pause to allow LCD EEPROM to program
 
-  serial.print("?D00000000000000000");       // define special characters
-  delay(300);                                  // delay to allow write to EEPROM
+  lcd_print("?D00000000000000000");  // define special characters
+  delay(300);                           // delay to allow write to EEPROM
   // see moderndevice.com for a handy custom char generator (software app)
-  serial.print("?f");                   // clear the LCD
+  lcd_clear();
   delay(10);
-  serial.print("...");
+  lcd_print("...");
 
 
   //crashes LCD without delay
-  serial.print("?D11010101010101010");
+  lcd_print("?D11010101010101010");
   delay(300);
 
-  serial.print("?D21818181818181818");
+  lcd_print("?D21818181818181818");
   delay(300);
 
-  serial.print("?D31c1c1c1c1c1c1c1c");
+  lcd_print("?D31c1c1c1c1c1c1c1c");
   delay(300);
 
-  serial.print("?D41e1e1e1e1e1e1e1e");
+  lcd_print("?D41e1e1e1e1e1e1e1e");
   delay(300);
 
-  serial.print("?D51f1f1f1f1f1f1f1f");
+  lcd_print("?D51f1f1f1f1f1f1f1f");
   delay(300);
 
-  serial.print("?D60000000000040E1F");
+  lcd_print("?D60000000000040E1F");
   delay(300);
 
-  serial.print("?D70000000103070F1F");
+  lcd_print("?D70000000103070F1F");
   delay(300);
 
-  serial.print("?c0");                  // turn cursor off
+  lcd_disable_cursor();
   delay(300);
 }
 
@@ -72,6 +72,18 @@ void lcd_set_brightness(byte brightness) {
 
 void lcd_set_cursor(byte x, byte y) {
   lcd_print("?x%02d?y%d", x, y);
+}
+
+void lcd_underline_cursor() {
+  lcd_print("?c3");
+}
+
+void lcd_blink_cursor() {
+  lcd_print("?c2");
+}
+
+void lcd_disable_cursor() {
+  lcd_print("?c0");
 }
 
 void lcd_print(char const *fmt, ... ) {
