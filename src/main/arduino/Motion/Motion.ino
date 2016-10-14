e5eb13754ed1cfdeae252a3f624227746261945a
Andy Condon <condon.andy@icloud.com>
2016-10-13 21:34:58 -0600

    Rounting commands to the LCD.
    Uses the internal address to determine it the command if for the LCD. Uses a 32 byte command buffer. Safely reads the data off I2C.
    
    Also, got rid of some LCD functions we won't use here. They'll be left to callers upstream to figure out.

--- src/main/arduino/Motion/Motion.ino
+++ src/main/arduino/Motion/Motion.ino
@@ -2,11 +2,14 @@
 #include "LCD.h"
 
 #define I2C_ADDRESS 0x1A
+#define I2C_BUFFER_SIZE 32
 #define LCD_TX_PIN 10
 #define LCD_ROWS 4
 #define LCD_COLUMNS 20
+#define LCD_ADDRESS 0x01
 
 LCD lcd = LCD(LCD_TX_PIN, LCD_ROWS, LCD_COLUMNS);
+char i2c_buffer[I2C_BUFFER_SIZE];
 
 void setup() {
   Wire.begin(I2C_ADDRESS);
@@ -30,23 +33,37 @@ void requestEvent() {
 }
 
 void receiveEvent(int howMany) {
-  lcd.clear_screen();
-  
-  if(0 >= Wire.available())
+  if (0 >= Wire.available())
   {
     return;
   }
-  
+
   byte internal_address = Wire.read();
-  
-  lcd.set_cursor(0, 0);
-  lcd.print("Int Addr: 0x%02X", internal_address);
-  lcd.set_cursor(1, 0);
-  lcd.print("Data:", internal_address);
-  lcd.set_cursor(2, 0);
-  
-  while (0 < Wire.available()) {
-    char c = Wire.read();
-    lcd.print("%c", c);
+  int data_size = get_data();
+
+  switch (internal_address) {
+    case LCD_ADDRESS:
+      lcd.print(i2c_buffer);
+      break;
   }
 }
+
+int get_data()
+{
+  int index = 0;
+
+  memset(i2c_buffer, 0x00, sizeof(i2c_buffer));
+
+  while (0 < Wire.available()) {
+    if (I2C_BUFFER_SIZE <= index)
+    {
+      break;
+    }
+
+    i2c_buffer[index] = Wire.read();
+    index++;
+  }
+
+  return index;
+}
+
