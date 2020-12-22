5c96cc514d028c4873da2b34b9fbbb5b9220a0c4
Andy Condon <condon.andy@icloud.com>
2020-12-21 20:32:33 -0700

    Use a ring buffer between the interrupt and main loop

--- arduino/Main/Main.ino
+++ arduino/Main/Main.ino
@@ -1,9 +1,11 @@
 #include <Wire.h>
+#include <RingBufCPP.h>
 #include "LCD.h"
 #include "Motor.h"
 
 #define I2C_ADDRESS 0x1A
 #define I2C_BUFFER_SIZE 32
+#define MAX_NUM_ELEMENTS 30
 #define LCD_TX_PIN 10
 #define LCD_ROWS 4
 #define LCD_COLUMNS 20
@@ -18,10 +20,18 @@
 LCD lcd = LCD(LCD_TX_PIN, LCD_ROWS, LCD_COLUMNS);
 char i2c_buffer[I2C_BUFFER_SIZE];
 
+struct Event
+{
+  byte internal_address;
+  byte direction;
+  byte speed;
+};
+
 // Remember to use Arduino pins, not physical ones
 Motor motor1 = Motor(2, 3);
 Motor motor2 = Motor(4, 5);
 volatile byte sensor_left, sensor_front, sensor_right;
+RingBufCPP<struct Event, MAX_NUM_ELEMENTS> buf;
 
 void setup()
 {
@@ -45,13 +55,27 @@ void setup()
 // L  :  0  5  5     F  :  0  5  5     R  :  0  5  5
 void loop()
 {
+  struct Event e;
+
+  while (buf.pull(&e))
+  {
+    switch (e.internal_address)
+    {
+    case MOTOR1_ADDRESS:
+      motor_event(&motor1, e.direction, e.speed);
+      break;
+    case MOTOR2_ADDRESS:
+      motor_event(&motor2, e.direction, e.speed);
+      break;
+    }
+  }
   lcd.set_cursor(1, 0);
   lcd.print("M1: %s", motor1.status());
   lcd.set_cursor(1, 9);
   lcd.print("M2: %s", motor2.status());
   lcd.set_cursor(3, 0);
   lcd.print("L:%03d F:%03d R:%03d", sensor_left, sensor_front, sensor_right);
-  delay(1000);
+  delay(2000);
 }
 
 void requestEvent()
@@ -70,33 +94,32 @@ void receiveEvent(int receive_size)
   {
     return;
   }
-
   byte internal_address = Wire.read();
-  switch (internal_address)
+
+  if (internal_address == MOTOR1_ADDRESS || internal_address == MOTOR2_ADDRESS)
   {
-  case MOTOR1_ADDRESS:
-    motor_event(&motor1, receive_size - 1);
-    break;
-  case MOTOR2_ADDRESS:
-    motor_event(&motor2, receive_size - 1);
-    break;
-  default:
-    return;
+    if (!Wire.available())
+    {
+      return;
+    }
+    byte direction = Wire.read();
+
+    if (!Wire.available())
+    {
+      return;
+    }
+    byte speed = Wire.read();
+
+    struct Event e;
+    e.internal_address = internal_address;
+    e.direction = direction;
+    e.speed = speed;
+    buf.add(e);
   }
 }
 
-void motor_event(Motor *motor, int receive_size)
+void motor_event(Motor *motor, byte direction, byte speed)
 {
-  if (!Wire.available())
-  {
-    return;
-  }
-  byte direction = Wire.read();
-  if (!Wire.available())
-  {
-    return;
-  }
-  byte speed = Wire.read();
   if (0x01 == direction)
   {
     motor->forward(speed);
